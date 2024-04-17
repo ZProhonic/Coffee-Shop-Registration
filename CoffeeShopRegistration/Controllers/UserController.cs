43bddb6003d0a4d1b753e033b759475434617206
Zack Prohonic <zackprohonic20@gmail.com>
2024-04-17 17:33:21 -0400

    Completed Coffe Shop Lab

---
+++ CoffeeShopRegistration/Controllers/UserController.cs
@@ -0,0 +1,31 @@
+﻿using CoffeeShopRegistration.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CoffeeShopRegistration.Controllers
+{
+    public class UserController : Controller
+    {
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Index(User user)
+        {
+            if (ModelState.IsValid)
+            {
+                return RedirectToAction("Welcome", user);
+            }
+
+            return View(user);
+        }
+
+        public IActionResult Welcome(User user)
+        {
+            return View(user);
+        }
+
+    }
+}
---
+++ CoffeeShopRegistration/Models/User.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace CoffeeShopRegistration.Models
+{
+    public class User
+    {
+        [Required(ErrorMessage = "First name is required")]
+        [Display(Name = "First Name")]
+        public string FirstName { get; set; }
+
+        [Required(ErrorMessage = "Last name is required")]
+        [Display(Name = "Last Name")]
+        
+        public string LastName { get; set; }
+
+        [Required(ErrorMessage = "Email address is required")]
+
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "Password is required")]
+
+        public string Password { get; set; }
+    }
+}
